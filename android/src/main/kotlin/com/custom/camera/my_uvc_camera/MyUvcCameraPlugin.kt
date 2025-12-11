ac8a7cb19fd1cc7bdd4bb0c06925dd294f6f3a3e
lzq <zhiqian_li@work@163.com>
2025-12-11 17:52:10 +0800

    change text

--- android/src/main/kotlin/com/custom/camera/my_uvc_camera/MyUvcCameraPlugin.kt
+++ android/src/main/kotlin/com/custom/camera/my_uvc_camera/MyUvcCameraPlugin.kt
@@ -21,7 +21,7 @@ class MyUvcCameraPlugin: FlutterPlugin, MethodCallHandler {
 
   override fun onMethodCall(call: MethodCall, result: Result) {
     if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      result.success("Android-${android.os.Build.VERSION.RELEASE}")
     } else {
       result.notImplemented()
     }
